6b877ebb084060e5792c93a296ca1f49c42f6cf1
Tomasz Szczepanowski <tomasz.szczepanowski@3s.pl>
2025-03-26 18:55:59 +0100

    log settings fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/automatic/
-./webui.sh --listen --api --use-cpu all --precision full --no-half --skip-torch-cuda-test >> webui.log
+./webui.sh --listen --api --use-cpu all --precision full --no-half --skip-torch-cuda-test > webui.log
